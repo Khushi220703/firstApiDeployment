4653c1c5b95a1bd72d8c8d7ea2b7067663c8bfcb
Khushi220703 <122461997+Khushi220703@users.noreply.github.com>
2024-08-10 19:00:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const mongoose = require("mongoose");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: https://first-deployment-one.vercel.app/', 
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
